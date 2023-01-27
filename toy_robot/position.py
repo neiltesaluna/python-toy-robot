837096b7fc0e9690b7e8ac3cabb628700722c3a0
Neil Tesaluna <neil.tesaluna@gmail.com>
2023-01-28 06:44:49 +1100

    fixed return type of location class

--- toy_robot/position.py
+++ toy_robot/position.py
@@ -1,11 +1,11 @@
 from enum import Enum, auto
 # determining the position of the robot
 class Location:
-    def __init__(self, x:int, y:int) -> tuple[int, int]:
+    def __init__(self, x:int, y:int):
         self.x = x
         self.y = y
 
-    def get_location(self):
+    def get_location(self) -> tuple[int, int]:
         return self.x, self.y
 
 # determining the direction the robot is facing
